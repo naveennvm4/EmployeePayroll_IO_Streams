1a20b7facaa5597d048bc1cbbb6f2451845a9336
naveennvm4 <naveennvm4@gmail.com>
2021-03-25 07:59:22 +0530

    Welcome to EmployeePayroll_IO_strems

---
+++ README.md
@@ -0,0 +1,1 @@
+#Welcome to EmployeePayroll_IO_Streams
---
+++ src/main/java/EmployeePayrollService.java
@@ -0,0 +1,5 @@
+public class EmployeePayrollService{
+    public static void main(String[] args) {
+        System.out.println("Welcome to IO stream Problem");
+    }
+}
