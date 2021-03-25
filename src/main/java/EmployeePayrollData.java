5e0d28f47d9236565561912c215eed56918a73ea
naveennvm4 <naveennvm4@gmail.com>
2021-03-25 08:06:59 +0530

    [Naveen]Added:Create EmployeePayroll service to read and write in console

---
+++ src/main/java/EmployeePayrollData.java
@@ -0,0 +1,15 @@
+class EmployeePayrollData {
+    public int id;
+    public String name;
+    public double salary;
+
+    public EmployeePayrollData(int id, String name, double salary) {
+        this.id = id;
+        this.name = name;
+        this.salary = salary;
+    }
+
+    public String toString() {
+        return "ID: " + id +"  Name: " + name + "  Salary: " + salary;
+    }
+}
--- src/main/java/EmployeePayrollService.java
+++ src/main/java/EmployeePayrollService.java
@@ -1,5 +1,32 @@
-public class EmployeePayrollService{
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class EmployeePayrollService {
+    public enum IOService {CONSOLE_IO, FILE_IO, DB_IO, REST_IO}
+    private ArrayList<EmployeePayrollData> empDetails;
+
+    public EmployeePayrollService(){
+        empDetails = new ArrayList<>();
+    }
+
+    private void readEmployeePayrollData(Scanner consoleInputReader) {
+        System.out.println("Enter id: ");
+        int id = consoleInputReader.nextInt();
+        System.out.println("Enter Name: ");
+        String name = consoleInputReader.next();
+        System.out.println("Enter Salary: ");
+        double salary = consoleInputReader.nextInt();
+        empDetails.add(new EmployeePayrollData(id, name, salary));
+    }
+
+    private void writeEmployeePayrollData() {
+        empDetails.stream().forEach(System.out::println);
+    }
+
     public static void main(String[] args) {
-        System.out.println("Welcome to IO stream Problem");
+        EmployeePayrollService employeePayrollService = new EmployeePayrollService();
+        Scanner sc = new Scanner(System.in);
+        employeePayrollService.readEmployeePayrollData(sc);
+        employeePayrollService.writeEmployeePayrollData();
     }
 }
